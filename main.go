6a7f67c9ae7558f1965a68eb43624639d00bfe44
natuyade <natuyade@tuta.io>
2026-04-11 21:37:26 +0900

    learning tea ui

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+
+    tea "charm.land/bubbletea/v2"
 )
 
 type Todo struct {
@@ -181,10 +183,111 @@ func arg_commands(Args []string) {
 		fmt.Println("this command not found\nType: todo (list | add <task> | delete <id> | complete <id>)")
 	}
 }
-
+/*
 func main() {
 
 	arg_commands(os.Args)
 
 	fmt.Println("Finished")
+	
+}*/
+
+type model struct{
+	numbers []string
+	cursor int
+	// mapはキーの型を自由に変えられる(stringならselected["yamada"]などで指定できる)
+	// [int]がキーの型, struct{}は値の型
+	selected map[int]struct{}
+}
+
+// init
+func (m model) Init() tea.Cmd {
+	// コマンドがない場合nil
+	return nil
+}
+
+// model init
+func initialModel() model {
+	return model{
+		numbers:  []string{"one","two","three"},
+
+		// 空のmapはmake()で作成
+		selected: make(map[int]struct{}),
+	}
+}
+
+// 操作があれば更新される
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	// 更新時押されていたkey
+	case tea.KeyPressMsg:
+
+		switch msg.String() {
+		// それがこのいずれかなら...
+		case "q", "ctrl+c":
+			// tea.Quitコマンドで終了
+			return m, tea.Quit
+			
+		case "up", "w":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		case "down", "s":
+			if m.cursor < len(m.numbers) -1 {
+				m.cursor++
+			}
+		case "enter":
+			// selected[key]に値が存在しているかを二つ目のvalueでみれる
+			// 存在していればtrue, いなければfalseが返る.
+
+			// boolで切り替えればいいのではとか思ったけど,
+			// 中身を取り出してboolを得るより外から中身の存在だけでboolを得れるため
+			// この方法が使われてるのだと思う. 多分だけど...
+
+			// 中身のvalueはどうでもいいので, 存在だけを確かめる
+			_, ok := m.selected[m.cursor]
+			if ok {
+				// 中身があればdelete(mapType, key)の値を削除
+				delete(m.selected, m.cursor)
+			} else {
+				// なければ, 存在boolをtrueにするために空の構造体を入れる
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() tea.View {
+	s := "number select test\n\n"
+
+	for i, number := range m.numbers {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+
+		selected := " "
+		_, ok := m.selected[i]
+		if ok {
+			selected = "✓"
+		}
+		// Sprintはto_stringへのformat
+		s += fmt.Sprintf("%s[%s] %s\n", cursor, selected, number)
+	} 
+
+	s += "\npress q to quit\n"
+
+    // Send the UI for rendering
+    return tea.NewView(s)
+}
+
+func main() {
+	p := tea.NewProgram(initialModel())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
 }
